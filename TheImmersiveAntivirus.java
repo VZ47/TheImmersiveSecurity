02a24585cf076c0ea3a8977c83f1a11ca9e89f6e
VZ47 <158313392+VZ47@users.noreply.github.com>
2024-02-19 16:29:35 +0300

    Add files via upload

---
+++ TheImmersiveAntivirus.java
@@ -0,0 +1,33 @@
+Second part.The Immersive Antivirus.
+Enable The Immersive Antivirus.
+
+Enable MachineLearning optimisation, security and system improvements. This should run automatic updates to prevent hacks, eleminate bugs and zero-day vulnerabilities.
+MachineLearning scripts for The Immersive Antivirus: AI generated GlobalID group #Tag, way of intrusion, collectibles, stack and note IP address branches and add to GlobalID sets (Include TraceRoute.set.IPs, group IPs with tags. Add AI generated text on what's and how hacked summary.
+
+
+Use MachineLearning for security updates to prevent hacks and self assigned security patches.
+Use https://www.cve.org/ for extra knowledge.
+
+The Immersive Antivirus Graphical Design (Command Line Design)
+
+
+Overview
+Line 1: TrackingID(ID of intrusion/hack/bug/vulnerability), Column 2:GlobalID Task number and actions made, Column 3: YandexGPT reply of legal authorities actions.
+Line 2 and so forth include the results
+
+Authority actions (root, admin and workers only). (Punish them bastards actions) Disable authority actions on root.
+Get access to intruders data. First option button "Emulate screen in hidden mode", Second option button "System dumb download in 5g.hidden mode"
+Upload a file to intruders computer. Button "Upload file to intruder"
+Enable.12g.powersupply.mode.computer.run.in.case.shutdown. Also, prevent.reboot, prevent.complete.shutdown. Button 'Control the intruders device in case the computer runs loud and we want to keep access to device'
+Stealth access mode.Button "Stealth mode" Use IPV6 server for access.
+Enable 2D command line reply of Command'Overview The Immersive Antivirus' with Line 1 table output. This shall run table of hacks, vulnerabilities and intrusions.
+Enable "Ask YandexGPT" command to provide information upon request.
+
+
+HELP SECTION UPON help click enter
+If you ran the program TopNotch security protocol has been installed. THE REST IS PLAYABLE.
+Type Overview The Immersive Antivirus to get antivirus data.
+After studying antivirus data you can pick options which you should study by reading the code of the app 
+'Ask YandexGPT command fulfills knowledge of intrusion
+
+
