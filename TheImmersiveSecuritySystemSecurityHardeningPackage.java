f4c8d5d7e1bb7d05608d363608e05e4b48b936b4
VZ47 <158313392+VZ47@users.noreply.github.com>
2024-02-19 16:02:14 +0300

    Add files via upload

---
+++ TheImmersiveSecuritySystemSecurityHardeningPackage.java
@@ -0,0 +1,29 @@
+data on his PC or server.
+
+    Enable 1ns encryption for root and 10ns for admin, 30ms encryption for workers/users admin to prevent data spoof, overlooking.
+
+        Disable IPV4
+
+This is only for root and admin users. Enable ALL TRAFFIC FIRST LAYER IPV6 PROTOCOL SOCKS5 (eg. 0.0.0.0:8888:9150:47(for root :6668 instead of :47)), CONNECTION TO SERVERS THEN SECOND LAYER ENCRYPTION LAYER WITH OnionRoutingNetwork.SOCKS5.marchrutization) PORT 9150 and the third layer top encryption WITH ROOT PRIVILEDGES USE PORT 6668, WITH admin 47 port by cloud I2P.
+        Force use DNS IPv6 2620:fe::fe
+
+
+Enable.NAT
+Enable.IPV6.NAT
+Disable.NAT.IPV4
+Enable.Double.NAT
+Enable.Double.NAT.IPV6
+Disable.Double.NAT.IPV4
+
+    Enable 1ns IP address swap for root and 10ns address swap for admin users, 30ms IP adresses swap every 30ms for worker and 50ms for users.
+
+Swap HTTP, HTTPS, SOCKS4, OpenVPN, SSH, Snowflake, IKEV2 and other ,traffic encapsulation and data transfer protocols), with connection protocols SOCKS5.
+
+    Enable 1ns for root, 10ns for admin data recypher on SSD, 30ms for workers.
+
+Make insignificant electrocurrents from various sources to prevent data leak and data access.
+
+AutoUpdate.from.TheSource
+Enable 0ns update mechanism
+
+
