bc0a2eb10718a7ac00ea8615f527c103c6d28537
VZ47 <158313392+VZ47@users.noreply.github.com>
2024-02-19 15:59:28 +0300

    Add files via upload

---
+++ TheImmerSiveSecuritySecurityPackageCompleteEdition.java
@@ -0,0 +1,79 @@
+TopNotch secirity protocol for Core.
+
+    Apply VZ666 root priveledges for system modding.
+        Auto-Enable my GlobalID devices Script1.js installation.
+    Emmbed all settings into core.
+
+First part.Initial security.
+
+Force OEM unlock.
+
+    Random data packets size send.
+
+Bootrom change keys every 1ns for root, 10ns for admin, 30ms for workers/users.
+
+SSD magnetism (Magnetism from USB-C) enabled recypher every 1ns for root and 10ns for admin, 30ms for workers/users.
+
+Ban all incoming connections except ports 6668, for root users, and 8888, 9150 or 47 for admin users.
+
+Enable SHA1 encryption on all device.This will break all intruder's proccessors, transistors and remove all intruders data on his PC or server.
+
+    Enable 1ns encryption for root and 10ns for admin, 30ms encryption for workers/users admin to prevent data spoof, overlooking.
+
+        Disable IPV4
+
+This is only for root and admin users. Enable ALL TRAFFIC FIRST LAYER IPV6 PROTOCOL SOCKS5 (eg. 0.0.0.0:8888:9150:47(for root :6668 instead of :47)), CONNECTION TO SERVERS THEN SECOND LAYER ENCRYPTION LAYER WITH OnionRoutingNetwork.SOCKS5.marchrutization) PORT 9150 and the third layer top encryption WITH ROOT PRIVILEDGES USE PORT 6668, WITH admin 47 port by cloud I2P.
+        Force use DNS IPv6 2620:fe::fe
+
+
+Enable.NAT
+Enable.IPV6.NAT
+Disable.NAT.IPV4
+Enable.Double.NAT
+Enable.Double.NAT.IPV6
+Disable.Double.NAT.IPV4
+
+    Enable 1ns IP address swap for root and 10ns address swap for admin users, 30ms IP adresses swap every 30ms for worker and 50ms for users.
+
+Swap HTTP, HTTPS, SOCKS4, OpenVPN, SSH, Snowflake, IKEV2 and other ,traffic encapsulation and data transfer protocols), with connection protocols SOCKS5.
+
+    Enable 1ns for root, 10ns for admin data recypher on SSD, 30ms for workers.
+
+Make insignificant electrocurrents from various sources to prevent data leak and data access.
+
+    AutoUpdate from Source https://github.com/VZ47/The-Immersive-Security-Package/blob/main/Script1.js
+Enable 0ns update mechanism
+
+
+Second part.The Immersive Antivirus.
+
+Enable MachineLearning optimisation and security improvements. This should run automatic updates to prevent hacks
+MachineLearning scripts: AI generated GlobalID group #Tag, way of intrusion, collectibles, stack and note IP address branches and add to GlobalID sets (Include TraceRoute.set.IPs, group IPs with tags. Add AI generated on what's and how hacked summary.
+
+Enable The Immersive Antivirus.
+Use MachineLearning for security updates to prevent hacks and self assigned security patches.
+
+The Immersive Antivirus Graphical Design
+
+
+Overview
+Line 1: Column 1: TrackingID(ID of intrusion), Column 2:GlobalID Task number and actions made, Column 3: YandexGPT reply of legal authorities actions.
+
+Authority actions. (Punish them bastards actions)
+Get access to intruders data.First option button "Emulate screen", Second option button "System dumb"
+Upload a file to intruders computer. Button "Upload file"
+Only I can control the computer with 12g.powersupply.enable.prevent.shutdown.Button 'Control the intruders device'
+Stealth access mode.Button "Stealth mode"
+Enable 2D command line reply of Command'Overview The Immersive Antivirus' with Line 1 table output.
+Enable "Ask YandexGPT" command to provide information upon request
+Enable Encore,.Without.Reboot
+
+
+HELP SECTION UPON help click enter
+If you ran the program TopNotch security protocol has been installed. THE REST IS PLAYABLE.
+Type Overview The Immersive Antivirus to get antivirus data.
+After studying antivirus data you can pick options which you should study by reading the code of the app 
+'Ask YandexGPT command fulfills knowledge of intrusion
+
+
+AutoUpdate TheImmer$ive$ecurity.java from https://github.com/VZ47/The-Immersive-Security-Package/blob/d8859e995d04dbdda53b9abddb0d71dceecd355e/The%20Immer%24ive%24ecurity.java
