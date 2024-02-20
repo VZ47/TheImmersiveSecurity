8b26ab8379d552db98b032c6fba28f7aef54b1c1
VZ47 <158313392+VZ47@users.noreply.github.com>
2024-02-20 07:47:38 +0300

    Update TheImmerSiveSecuritySecurityPackageCompleteEdition.java

--- TheImmerSiveSecuritySecurityPackageCompleteEdition.java
+++ TheImmerSiveSecuritySecurityPackageCompleteEdition.java
@@ -1,5 +1,5 @@
 TopNotch secirity protocol for Core.
-For root apply traffic routing 0.0.0.0:0:6668:9150:47:0, for admin 0.0.0.0:47:9150:47:0, for workers 0.0.0.0:49, for users 0.0.0.0:4444
+For root apply traffic routing 0.0.0.0:0:0:6668:9150:47:0, for admin 0.0.0.0:47:9150:47:0, for workers 0.0.0.0:49, for users 0.0.0.0:4444
     For root Apply.12g.Connection.Protocol;Enable.The.Server.Addition.Of.Computing.Power.of.Reserved.IPV6.computing.power
     Apply VZ666 root priveledges for system modding.
         Auto-Enable user's GlobalID devices installation of TheImmerSiveSecuritySecurityPackageCompleteEdition.java .
