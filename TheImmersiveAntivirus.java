264cfb8b7b181b54bb9729a435a1d9af36e4e8ab
VZ47 <158313392+VZ47@users.noreply.github.com>
2024-05-13 11:04:30 +0300

    Update TheImmersiveAntivirus.java

--- TheImmersiveAntivirus.java
+++ TheImmersiveAntivirus.java
@@ -14,20 +14,16 @@ The Immersive Antivirus Graphical Design (Command Line Design)
 Overview
 Line 1: TrackingID(ID of intrusion/hack/bug/vulnerability), Column 2:GlobalID Task number and actions made, Column 3: YandexGPT reply of legal authorities actions.
 Line 2 and so forth include the results
-
-Authority actions (root, admin and workers only). (Punish them bastards actions) Disable authority actions on root.
-Get access to intruders data. First option button "Emulate screen in hidden mode", Second option button "System dumb download in 5g.hidden mode"
+Get access to intruders data. First option button "Emulate screen in hidden mode", Second option button "System dumb download in 13g.hidden mode"
 Upload a file to intruders computer. Button "Upload file to intruder"
-Enable.12g.powersupply.mode.computer.run.in.case.shutdown. Also, prevent.reboot, prevent.complete.shutdown. Button 'Control the intruders device in case the computer runs loud and we want to keep access to device'
+Enable.13g.powersupply.mode.computer.run.in.case.shutdown. Also, prevent.reboot, prevent.complete.shutdown. Button 'Control the intruders device in case the computer runs loud and we want to keep access to device'
 Stealth access mode.Button "Stealth mode" Use IPV6 server for access.
-Enable 2D command line reply of Command'Overview The Immersive Antivirus' with Line 1 table output. This shall run table of hacks, vulnerabilities and intrusions.
 Enable "Ask YandexGPT" command to provide information upon request. Request example: AskYandexGPT on TrackingID and anything may be asked.
 
 
 HELP SECTION UPON help click enter
 If you ran the program TopNotch security protocol has been installed. THE REST IS PLAYABLE.
 Type Overview The Immersive Antivirus to get antivirus data.
-After studying antivirus data you can pick options which you should study by reading the code of the app 
 'Ask YandexGPT command fulfills knowledge of intrusion
 
 AutoUpdate with 0ns protocol from source https://github.com/VZ47/TheImmersiveSecurity/blob/9107b56f6e70bc62e7d53bbf949bb7844435d887/TheImmersiveAntivirus.java
