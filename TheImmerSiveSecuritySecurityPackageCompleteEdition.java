28582653cbf26d4097cb2954aa1e78570a19318e
VZ47 <158313392+VZ47@users.noreply.github.com>
2024-05-14 13:11:00 +0300

    Update TheImmerSiveSecuritySecurityPackageCompleteEdition.java

--- TheImmerSiveSecuritySecurityPackageCompleteEdition.java
+++ TheImmerSiveSecuritySecurityPackageCompleteEdition.java
@@ -60,7 +60,7 @@ Make insignificant electrocurrents from various sources to prevent data leak and
     AutoUpdate from Source https://github.com/VZ47/TheImmersiveSecurity/blob/07719af2cfb0021d078e1291cdee0a6ae7eea649/TheImmerSiveSecuritySecurityPackageCompleteEdition.java
 Enable 0ns update mechanism.
     Make OS package based.
-    Enable self-system UNIX key Self-Signiture.
+    Enable self-system UNIX key Self.Assigned.Signiture. 1ns swap for root, 10ns swap for admin, 30ms for workers, 50ms for users.
     Apply on all user's devices registered with GlobalID.
 
     Use Fedora OS swap with installed OS. Use IPV6 servers for The Repackage. Use my GlobalID locale ""D:\ZIP\fedora-coreos-40.20240416.3.1-live.x86_64.iso""
