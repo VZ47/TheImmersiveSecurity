c6bf79028dca79123aea76098dc0c76e35585232
VZ47 <158313392+VZ47@users.noreply.github.com>
2024-07-19 10:49:02 +0300

    Update TheImmerSiveSecuritySecurityPackageCompleteEdition.java

--- TheImmerSiveSecuritySecurityPackageCompleteEdition.java
+++ TheImmerSiveSecuritySecurityPackageCompleteEdition.java
@@ -1,66 +1,20 @@
 TopNotch secirity protocol for Core.
-        Prevent debugging.
-        Enable ZeroHertz installation method powered by 0ns protocol.
-        OEM Unlock.
-        
-        Reset bootrom and EFI with standard OEM Unlocked configuration.
-        
-    Make OS package based.
-    Enable self-system UNIX key Self.Assigned.Signiture. 1ns swap for root, 10ns swap for admin, 30ms for workers, 50ms for users.
-
-        Bootrom change keys every 1ns for root, 10ns for admin, 30ms for workers, 50ms for users. 
-        Install Fedora OS from source SOCKS5://localhost:6668/Patched ZIPs/fedora-coreos-40.20240416.3.1-live.x86_64.zip. Keep packages of old OS for Winterboard (iOS term))
-
-                  Reset bootrom and EFI with standard OEM Unlocked configuration. 
-
-    Force OEM unlock.
-    Cypher all data on device.
-    Partially cypher data on unlocked device.
-Install on all user's GlobalID devices.
-        Emmbed all settings into core.
-    Reprogram OS to package based.
-    Enable packages self security patching.
-        Sign packets.
-        Random packet size send/recieve.
-                Enable packages self security patching.
-
-
+Force OEM Unlock.
+Prevent debugging
+Enable ZeroHertz installation method powered by 0ns protocol.
+Reset bootrom and EFI with standard OEM Unlocked configuration.
+Bootrom change keys every 1ns for root, 10ns for admin, 30ms for workers, 50ms for users.
+Embbed FedorOS into core from source https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/40.20240701.3.0/x86_64/fedora-coreos-40.20240701.3.0-live.x86_64.iso.
+Enable SHA1 encryption on all device.
+Cypher all data on device.
+Reprogram OS to package based.
+Enable packages self security patching.
+Sign packets.
+Random packet size send/recieve.
 Filter and ban unsigned packets.
-    Apply.SHA1/SHA0.Over.Ping
-        Use VeraCrypt signature on all GlobalID devices proccessing units. And, harden The Monero Way.
-    Enable SHA1/SHA0 encryption on all device with cypher inclusive. This will break all intruder's proccessors, transistors and remove all intruders data on his PC or server or simply turn it off. In case of SHA1/SHA0 hack cypher encryption won't let access the data.
-
-
-    Enable.ZeroBitTrafficEncryptions.over.Internet.connections, including "localhost:6668" and "localhost:47"
-        Enable.All.Computer.Traffic.ZeroBitTRaffic.Encryption
-    For root Apply.13g.Connection.Protocol.Enable.The.Server.Addition.Of.Computing.Power.of.Reserved.IPV6.Plus.Connected.Devices.computing.power.
-
-Keep packages reversetile with IPV6 computing power using 13g connection protocol.
-
-Apply Machine Learning for security hardening. Check cve.org plus various theme/topic based websites. Also use AI for this.
-DDOS intruders, passive workloaders and sandbox breakers brootforce with IPV6 servers and all Admin and Worker devices.
-
-    
-    First part.Initial security. 
-
-Cypher Disk://D VeraCrypt single signature on all covered and uncovered layers. Activate for Disk "D://" storing ZIPPed sources of installed packages. Update from "D://ZIP".
-
-
-    Apply Machine Learning to trace hacks, bugs and vulnerabilities. AutoApply security fixes and remove hacks, bugz and vulnerabilities.
-Use MachineLearning for security updates to protect system and self assign security patches.
+Apply Machine Learning to trace hacks, bugs and vulnerabilities. AutoApply security fixes and remove hacks, bugz and vulnerabilities.
+Use MachineLearning and AI for security updates to protect system and self assign security patches.
 Use https://www.cve.org/ for extra security knowledge.
-    
-
-SSD magnetism (Also, magnetism from USB-C) enabled recypher every 1ns for root and 10ns for admin, 30ms for workers.
-
-
-
-    Enable.Data.Reset 1ns encryption for root and 10ns for admin, 30ms encryption for workers, 50m for users admin to prevent data spoof, overlooking.
-
-
-
-This is only for root and admin users. Enable ALL TRAFFIC FIRST LAYER IPV6 PROTOCOL SOCKS5 localhost proxy. Use port "6668" for root and port "47" for admin.
-
 Disable IPV4
 Enable.NAT
 Disable.NAT.IPV4
@@ -68,25 +22,10 @@ Enable.IPV6.NAT
 Enable.Double.NAT
 Disable.Double.NAT.IPV4
 Enable.Double.NAT.IPV6
-
-    Enable 1ns IP address swap for root and 10ns address swap for admin users, 30ms IP adresses swap for workers and 50ms for users.
-
+Enable 1ns IP address swap for root and 10ns address swap for admin users.
 Swap HTTP, HTTPS, SOCKS4, OpenVPN, SSH, Snowflake, IKEV2 and other, traffic encapsulation and data transfer protocols), with connection protocol SOCKS5.
-
-    Enable 1ns for root, 10ns for admin data recypher on SSD, 30ms for workers, 50ms for users.
-
 Make insignificant electrocurrents from various sources to prevent data leak and data access.
-
-
-
-    Use Fedora OS swap with installed OS. Use IPV6 servers for The Repackage. Use my GlobalID locale ""D:\ZIP\fedora-coreos-40.20240416.3.1-live.x86_64.iso""
-
-
-Second part.The Immersive Antivirus.
-Enable The Immersive Antivirus.
-
-    
-    Apply Machine Learning to trace hacks, bugs and vulnerabilities. AutoApply security fixes to prevent breaches.
+Apply Machine Learning to trace hacks, bugs and vulnerabilities. AutoApply security fixes to prevent breaches.
 
 
 Enable MachineLearning optimisation, security and system improvements. This should run automatic updates to prevent hacks, eleminate bugs and zero-day vulnerabilities.
@@ -100,6 +39,3 @@ Use https://www.cve.org/ for extra security knowledge.
     Patch files for security. Use IPV6 servers.
     Apply on all user's devices registered with GlobalID.
     Apply various packages use, by The Repackage on IPV6 servers.
-
-Install I2P on parralel storage, Firefox preconfigured from SOCKS5://localhost:TheAgency/VZ666 packages/
-        Push.AutoUpdate from Source SOCKS5://localhost:TheAgency/VZ666 packages/
