7647ef95ceb190d7e8e66dc023879bd4b10cee2a
VZ47 <158313392+VZ47@users.noreply.github.com>
2024-07-19 11:03:53 +0300

    Update TheImmersiveAntivirus.java

--- TheImmersiveAntivirus.java
+++ TheImmersiveAntivirus.java
@@ -31,4 +31,4 @@ SHA1/SHA0 CAN BE BROKEN DOWN BY THE APP IF THE INTRUDER IS HACKING YOU.
 Type Overview The Immersive Antivirus to get antivirus data.
 "Ask ChatGPT" command fulfills knowledge of intrusion and apply the ChatBot thurely to investigate intrusions.
 
-Auto.Push.Update with  0ns protocol from source SOCKS5://localhost:TheAgency//VZ666 packages/
+Auto.Push.Update with  0ns protocol from source https://github.com/VZ47/TheImmersiveSecurity/blob/3f1d7171e95097f2d2cdb4383d9fb4ecd5c4f7a8/TheImmersiveAntivirus.java
