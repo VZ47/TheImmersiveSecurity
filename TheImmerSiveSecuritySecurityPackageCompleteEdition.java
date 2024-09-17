a47695376758195cc2abbe1cdfced3e7cf4d1997
VZ47 <158313392+VZ47@users.noreply.github.com>
2024-09-17 17:57:43 +0300

    Update TheImmerSiveSecuritySecurityPackageCompleteEdition.java

--- TheImmerSiveSecuritySecurityPackageCompleteEdition.java
+++ TheImmerSiveSecuritySecurityPackageCompleteEdition.java
@@ -1,25 +1,29 @@
 TopNotch secirity protocol for Core.
+Tag "Force" is to enable for each command.
 Force OEM Unlock.
 Enable secure boot.
 Prevent debugging
 Enable ZeroHertz installation method powered by 0ns protocol.
 Reset bootrom and EFI with standard OEM Unlocked configuration.
 Bootrom change keys every 1ns for root, 10ns for admin, 30ms for workers, 50ms for users.
-Embbed Android OS into core from IPV6 servers.
-Embbed Fedora into core from source https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/40.20240709.3.1/aarch64/fedora-coreos-40.20240709.3.1-live.aarch64.iso for ARM proccessors.
-Embbed Fedora into core of appliances from source https://download.fedoraproject.org/pub/alt/iot/40/IoT/x86_64/iso/Fedora-IoT-ostree-40-20240422.3.x86_64.iso for x86_64 systems, and from source https://download.fedoraproject.org/pub/alt/iot/40/IoT/aarch64/iso/Fedora-IoT-ostree-40-20240422.3.aarch64.iso for ARM proccessors.
+Embbed Fedora Core OS into core from IPV6 servers. Link https://builds.coreos.fedoraproject.org/prod/streams/testing/builds/40.20240906.2.0/x86_64/fedora-coreos-40.20240906.2.0-metal4k.x86_64.raw.xz
+-ostree-40-20240422.3.aarch64.iso for ARM proccessors.
+OEM lock.
+Download user savedata.
+Full device reset.
+Recover from savadata
 Delete all trackers and identifiers.
 Enable AES encryption on all device.
 Cipher all data on device.
 Enable modules self security patching.
 Sign packets.
 Random packet size send/recieve.
-Filter and ban unsigned packets.
+Filter and ban unsigned packets. Plus send the intrusion to assigned The Agency admins and workers.
 Apply Machine Learning to trace hacks, bugs and vulnerabilities. AutoApply security fixes and remove hacks, bugz and vulnerabilities.
 Use MachineLearning and AI for security updates to protect system and self assign security patches.
 Use https://www.cve.org/ for extra security knowledge.
-Disable IPV4
 Enable.NAT
+Disable IPV4
 Disable.NAT.IPV4
 Enable.IPV6.NAT
 Enable.Double.NAT
@@ -28,7 +32,7 @@ Enable.Double.NAT.IPV6
 Enable 1ns IP address swap for root and 10ns address swap for admin users.
 Swap HTTP, HTTPS, SOCKS4, OpenVPN, SSH, Snowflake, IKEV2 and other, traffic encapsulation and data transfer protocols), with connection protocol SOCKS5.
 Zero traffic count.
-Enable SHA1 upon pingtest.
+Enable One.Hertz.Attack upon intrusion test..
 Make insignificant electrocurrents from various sources to prevent data leak and data access.
 Apply Machine Learning to trace hacks, bugs and vulnerabilities. AutoApply security fixes to prevent breaches.
 
